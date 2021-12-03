2f8976fe0c7181bc594e2289f412ea839f039340
francismsc <frankymccosta@gmail.com>
2021-12-03 15:51:10 +0000

    More movement

--- O Folião/Assets/Game.cs
+++ O Folião/Assets/Game.cs
@@ -7,58 +7,20 @@ public class Game : MonoBehaviour
     public Player player;
     public Movement move;
 
-    bool gameon = true;
-    bool moveTurn = true;
-    bool timetomove = true;
-    GameObject objecto;
-    private Node node;
-    private List<GameObject> list;
-    public Material mate;
-
     void Awake()
     {
-
+        
     }
 
     private void Update()
     {
-        if(timetomove)
+        if (move.Movingtime() == true)
         {
-            list = player.Position().GetComponent<Node>().GetOptions();
-            foreach (GameObject nodes in list)
-            {
-                nodes.GetComponent<Node>().HighLight();
-            }
-            if (Input.GetMouseButtonUp(0))
-            {
-
-                // Create a ray from the current mouse position
-                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-                RaycastHit hit;
-                if (Physics.Raycast(ray, out hit))
-                {
-
-                    if (hit.transform.tag == "Point");
-                    {
-                        foreach(GameObject nodes in list)
-                        {
-                            if (GameObject.ReferenceEquals(hit.transform.GetChild(0).gameObject, nodes))
-                            {
-                                foreach (GameObject points in list)
-                                {
-                                    points.GetComponent<Node>().BacktoNormal();
-                                }
-
-                                move.Move(player, hit.transform.GetChild(0).gameObject);
-
-                            }
-                        }
-
-                    }
-                }
-            }
-            
-
+            GetComponent<Movement>().enabled = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            move.TimetoMove();
         }
     }
     private void Start()
--- O Folião/Assets/Movement.cs
+++ O Folião/Assets/Movement.cs
@@ -4,12 +4,91 @@ using UnityEngine;
 
 public class Movement: MonoBehaviour
 {
-
+    public Player player;
+    bool timetomove = true;
+    private List<GameObject> list;
+    public Material mate;
+    bool moving = false;
+    GameObject child;
+    bool choice = false;
     public void Move(Player player,GameObject nextpoint)
     {
-        player.ChangePosition(nextpoint);   
+        
+        player.ChangePosition(nextpoint);
+        
     }
 
-    
+    public bool Movingtime()
+    {
+        return timetomove;
+    }
+
+    public void TimetoMove()
+    {
+        timetomove = true;
+    }
+    public void Update()
+    {
+        if (timetomove)
+        {
+            list = player.Position().GetComponent<Node>().GetOptions();
+            foreach (GameObject nodes in list)
+            {
+                nodes.GetComponent<Node>().HighLight();
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+
+                // Create a ray from the current mouse position
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+
+                    if (hit.transform.tag == "Point")
+                    {
+                        foreach (GameObject nodes in list)
+                        {
+                            if (GameObject.ReferenceEquals(hit.transform.GetChild(0).gameObject, nodes))
+                            {
+                                foreach (GameObject points in list)
+                                {
+                                    points.GetComponent<Node>().BacktoNormal();
+                                }
+                                child = hit.transform.GetChild(0).gameObject;
+                                Move(player, child);
+                                moving = true;
+                                choice = true;
+
+                            }
+                        }
+
+                    }
+                }
+            }
+
+            if (moving == true && player.gameObject.transform.position != child.transform.position)
+            {
+                player.gameObject.transform.position = Vector3.MoveTowards(player.gameObject.transform.position, child.transform.position, 5 * Time.deltaTime);
+            }
+            else if (player.gameObject.transform.position == null || (player.gameObject.transform.position == child.transform.position && choice == false))
+            {
+                moving = false;
+            }
+            else if (player.gameObject.transform.position == child.transform.position && choice == true)
+            {
+                choice = false;
+                moving = false;
+                timetomove = false;
+            }
+
+
+
+
+
+        }
+    }
+
+
 
 }
