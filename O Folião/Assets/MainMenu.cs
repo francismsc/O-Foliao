535fbfc74756064edf0b2edb7514147ddf3882e6
Carolina Bastos <carol1999b@gmail.com>
2021-12-08 18:49:28 +0000

    Add events and fixe failscene

--- O Folião/Assets/MainMenu.cs
+++ O Folião/Assets/MainMenu.cs
@@ -18,6 +18,6 @@ public class MainMenu : MonoBehaviour
 
     public void BackToMainMenu()
     {
-        SceneManager.LoadScene("Main Menu");
+        SceneManager.LoadScene("Start Menu");
     }
 }
