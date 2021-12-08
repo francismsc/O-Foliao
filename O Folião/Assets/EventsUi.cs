ba3655810318620a16947ed6cbcd263a49cc7e63
francismsc <frankymccosta@gmail.com>
2021-12-08 18:45:12 +0000

    Add Resultschoices

--- O Folião/Assets/EventsUi.cs
+++ O Folião/Assets/EventsUi.cs
@@ -18,6 +18,9 @@ public class EventsUi : MonoBehaviour
     public GameObject choice1button1;
     public GameObject choice1button2;
     public GameObject description2;
+
+    public GameObject result;
+
     public Events evenaux;
     public Player player;
     public void eventUi(Events even)
@@ -47,32 +50,47 @@ public class EventsUi : MonoBehaviour
 
     }
 
+    public void resultcanvas()
+    {
+        result  .SetActive(true);
+    }
+
     public void choice11()
     {
         player.ChangeStats(player, evenaux.decisions[0].alcoolD, evenaux.decisions[0].funD, evenaux.decisions[0].socialD, evenaux.decisions[0].moneyD, evenaux.decisions[0].energyD);
         choices2.SetActive(false);
+        result.GetComponentInChildren<Text>().text = evenaux.decisions[0].stringF;
+        resultcanvas();
     }
 
     public void choice12()
     {
         player.ChangeStats(player, evenaux.decisions[1].alcoolD, evenaux.decisions[1].funD, evenaux.decisions[1].socialD, evenaux.decisions[1].moneyD, evenaux.decisions[1].energyD);
         choices2.SetActive(false);
+        result.GetComponentInChildren<Text>().text = evenaux.decisions[1].stringF;
+        resultcanvas();
     }
 
     public void choice31()
     {
         player.ChangeStats(player, evenaux.decisions[0].alcoolD, evenaux.decisions[0].funD, evenaux.decisions[0].socialD, evenaux.decisions[0].moneyD, evenaux.decisions[0].energyD);
         choices3.SetActive(false);
+        result.GetComponentInChildren<Text>().text = evenaux.decisions[0].stringF;
+        resultcanvas();
     }
     public void choice32()
     {
         player.ChangeStats(player, evenaux.decisions[1].alcoolD, evenaux.decisions[1].funD, evenaux.decisions[1].socialD, evenaux.decisions[1].moneyD, evenaux.decisions[1].energyD);
         choices3.SetActive(false);
+        result.GetComponentInChildren<Text>().text = evenaux.decisions[1].stringF;
+        resultcanvas();
     }
     public void choice33()
     {
         player.ChangeStats(player, evenaux.decisions[2].alcoolD, evenaux.decisions[2].funD, evenaux.decisions[2].socialD, evenaux.decisions[2].moneyD, evenaux.decisions[2].energyD);
         choices3.SetActive(false);
+        result.GetComponentInChildren<Text>().text = evenaux.decisions[2].stringF;
+        resultcanvas();
     }
 
 
