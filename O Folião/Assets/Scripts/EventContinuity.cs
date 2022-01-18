0f35122fc7f1f30d58ce1c8bb6f766e08e26b638
francismsc <frankymccosta@gmail.com>
2022-01-18 18:03:02 +0000

    Add EventContinuity Object

--- O Folião/Assets/Scripts/EventContinuity.cs
+++ O Folião/Assets/Scripts/EventContinuity.cs
@@ -4,32 +4,32 @@ using UnityEngine;
 using System;
 
 
-    [CreateAssetMenu(fileName = "New EventContinuity/StoryLine", menuName = "Events/New Continuity")]
-    public class EventContinuity : ScriptableObject
-    {
-        public enum DayOfWeek { Day1, Day2, Day3, Day4, Day5 };
+[CreateAssetMenu(fileName = "New EventContinuity/StoryLine", menuName = "Events/New Continuity")]
+public class EventContinuity : ScriptableObject
+{
+    public enum DayOfWeek { Day1, Day2, Day3, Day4, Day5 };
 
-        [SerializeField] private DayOfWeek _dayOfWeek;
-        public DayOfWeek dayOfWeek { get { return _dayOfWeek; } }
+    [SerializeField] private DayOfWeek _dayOfWeek;
+    public DayOfWeek dayOfWeek { get { return _dayOfWeek; } }
 
-        public enum Local { Stages, Bars };
+    public enum Local { Stages, Bars };
 
-        [SerializeField] private Local _local;
-        public Local local { get { return _local; } }
+    [SerializeField] private Local _local;
+    public Local local { get { return _local; } }
 
-        public enum TimeOfDay { Morning, Afternoon, Night };
+    public enum TimeOfDay { Morning, Afternoon, Night };
 
-        [SerializeField] private TimeOfDay _timeOfDay;
-        public TimeOfDay timeOfDay { get { return _timeOfDay; } }
+    [SerializeField] private TimeOfDay _timeOfDay;
+    public TimeOfDay timeOfDay { get { return _timeOfDay; } }
 
-        [SerializeField] private Events _nextEvent;
-        public Events nextEvent { get { return _nextEvent; } }
-
-
+    [SerializeField] private Events _theEvent;
+    public Events theEvent { get { return _theEvent; } }
 
 
-        //DIA
-        //Local
-        //time
-    }   //Event
+
+
+    //DIA
+    //Local
+    //time
+}   //Event
     
--- O Folião/Assets/Scripts/Events.cs
+++ O Folião/Assets/Scripts/Events.cs
@@ -6,7 +6,7 @@ using System;
 
     [Serializable()]
     public struct Decisions
-    {
+{ 
         [SerializeField] private string _decisionDescription;
         public string decisionDescription { get { return _decisionDescription; } }
 
@@ -32,100 +32,106 @@ using System;
         public FailedEvent[] failedEvent { get { return _failedEvent; } }
 
     }
-    [Serializable()]
-    public struct SucessEvent
-    {
-        [SerializeField] private string _description;
-        public string description { get { return _description; } }
+[Serializable()]
+public struct SucessEvent
+{
+    [SerializeField] private string _description;
+    public string description { get { return _description; } }
 
-        [SerializeField] private int _timePassed;
-        public int timePassed { get { return _timePassed; } }
+    [SerializeField] private int _timePassed;
+    public int timePassed { get { return _timePassed; } }
 
-        [SerializeField] private int _alcoolPlus;
-        public int alcoolPlus { get { return _alcoolPlus; } }
+    [SerializeField] private int _alcoolPlus;
+    public int alcoolPlus { get { return _alcoolPlus; } }
 
-        [SerializeField] private int _funPlus;
-        public int funPlus { get { return _funPlus; } }
+    [SerializeField] private int _funPlus;
+    public int funPlus { get { return _funPlus; } }
 
-        [SerializeField] private int _socialPlus;
-        public int socialPlus { get { return _socialPlus; } }
+    [SerializeField] private int _socialPlus;
+    public int socialPlus { get { return _socialPlus; } }
 
-        [SerializeField] private int _moneyPlus;
-        public int moneyPlus { get { return _moneyPlus; } }
+    [SerializeField] private int _moneyPlus;
+    public int moneyPlus { get { return _moneyPlus; } }
 
-        [SerializeField] private int _energyPlus;
-        public int energyPlus { get { return _energyPlus; } }
+    [SerializeField] private int _energyPlus;
+    public int energyPlus { get { return _energyPlus; } }
 
-        [SerializeField] private Events[] _moreDecisionsStages;
-        public Events[] moreDecisionsStages { get { return _moreDecisionsStages; } }
-    }
+    [SerializeField] private Events[] _moreDecisionsStages;
+    public Events[] moreDecisionsStages { get { return _moreDecisionsStages; } }
 
-
-    [Serializable()]
-    public struct FailedEvent
-    {
-        [SerializeField] private string _description;
-        public string description { get { return _description; } }
-
-        [SerializeField] private int _timePassed;
-        public int timePassed { get { return _timePassed; } }
-
-        [SerializeField] private int _alcoolPlus;
-        public int alcoolPlus { get { return _alcoolPlus; } }
-
-        [SerializeField] private int _funPlus;
-        public int funPlus { get { return _funPlus; } }
-
-        [SerializeField] private int _socialPlus;
-        public int socialPlus { get { return _socialPlus; } }
-
-        [SerializeField] private int _moneyPlus;
-        public int moneyPlus { get { return _moneyPlus; } }
-
-        [SerializeField] private int _energyPlus;
-        public int energyPlus { get { return _energyPlus; } }
-
-        [SerializeField] private Events[] _moreDecisionsStages;
-        public Events[] moreDecisionsStages { get { return _moreDecisionsStages; } }
-    }
-
-    
-    [CreateAssetMenu(fileName = "New Event", menuName = "Events/new Event")]
-    public class Events : ScriptableObject
-    {
-
-        [SerializeField] private String _event = String.Empty;
-        public String Event { get { return _event; } }
-
-        [SerializeField] private Sprite _background = null;
-        public Sprite background { get { return _background; } }
-
-        [SerializeField] Decisions[] _decisions = null;
-        public Decisions[] decisions { get { return _decisions; } }
+    [SerializeField] private EventContinuity _nextEventStoryLine;
+    public EventContinuity nextEventStoryLine { get { return _nextEventStoryLine; } }
+}
 
 
-        //Tags only for easy finding and placing of the object
-        public enum Alcool { Low30, Normal, High80 };
+[Serializable()]
+public struct FailedEvent
+{
+    [SerializeField] private string _description;
+    public string description { get { return _description; } }
 
-        [SerializeField]
-        private Alcool alcool;
+    [SerializeField] private int _timePassed;
+    public int timePassed { get { return _timePassed; } }
 
-        public enum Energy { Low30, Normal };
+    [SerializeField] private int _alcoolPlus;
+    public int alcoolPlus { get { return _alcoolPlus; } }
 
-        [SerializeField]
-        private Energy energy;
+    [SerializeField] private int _funPlus;
+    public int funPlus { get { return _funPlus; } }
 
-        public enum TimeOfDay { Morning, Afternoon, Night };
+    [SerializeField] private int _socialPlus;
+    public int socialPlus { get { return _socialPlus; } }
 
-        [SerializeField] private TimeOfDay _timeOfDay;
-        public TimeOfDay timeOfDay { get { return _timeOfDay; } }
+    [SerializeField] private int _moneyPlus;
+    public int moneyPlus { get { return _moneyPlus; } }
 
-        
-        public enum DayOfWeek { Day1, Day2, Day3, Day4, Day5 };
+    [SerializeField] private int _energyPlus;
+    public int energyPlus { get { return _energyPlus; } }
 
-        [SerializeField] private DayOfWeek _dayOfWeek;
-        public DayOfWeek dayOfWeek { get { return _dayOfWeek; } }
+    [SerializeField] private Events[] _moreDecisionsStages;
+    public Events[] moreDecisionsStages { get { return _moreDecisionsStages; } }
+
+    [SerializeField] private EventContinuity _nextEventStoryLine;
+    public EventContinuity nextEventStoryLine {get { return _nextEventStoryLine; } }
+}
+
+
+[CreateAssetMenu(fileName = "New Event", menuName = "Events/new Event")]
+public class Events : ScriptableObject
+{
+
+    [SerializeField] private String _event = String.Empty;
+    public String Event { get { return _event; } }
+
+    [SerializeField] private Sprite _background = null;
+    public Sprite background { get { return _background; } }
+
+    [SerializeField] Decisions[] _decisions = null;
+    public Decisions[] decisions { get { return _decisions; } }
+
+
+    //Tags only for easy finding and placing of the object
+    public enum Alcool { Low30, Normal, High80 };
+
+    [SerializeField]
+    private Alcool alcool;
+
+    public enum Energy { Low30, Normal };
+
+    [SerializeField]
+    private Energy energy;
+
+    public enum TimeOfDay { Morning, Afternoon, Night };
+
+    [SerializeField] private TimeOfDay _timeOfDay;
+    public TimeOfDay timeOfDay { get { return _timeOfDay; } }
+
+
+    public enum DayOfWeek { Day1, Day2, Day3, Day4, Day5 };
+
+    [SerializeField] private DayOfWeek _dayOfWeek;
+    public DayOfWeek dayOfWeek { get { return _dayOfWeek; } }
 
 
 
-    }
+}
